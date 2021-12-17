e89c6be5116b1c9f8cf686f05f2fba2a8a2ca35e
Eyre_S <sukazyo@outlook.com>
2021-12-17 13:58:13 +0800

    规范文件编码与换行符，规范编辑规范，添加 CLI 项目

--- SukWs.GitHub_Label_Manager.Core/Data/Label.cs
+++ SukWs.GitHub_Label_Manager.Core/Data/Label.cs
@@ -1,20 +1,20 @@
-﻿namespace SukWs.GitHub_Label_Manager.Core.Data
+namespace SukWs.GitHub_Label_Manager.Core.Data
 {
 	internal class Label
 	{
 
-		public readonly string uuid;
+		public readonly string UUID;
 
-		public string name;
-		public string color;
-		public string description;
-
+		public string Name;
+		public string Color;
+		public string Description;
+		
 		public Label(string name, string color, string description, string uuid)
 		{
-			this.uuid = uuid;
-			this.name = name;
-			this.color = color;
-			this.description = description;
+			UUID = uuid;
+			Name = name;
+			Color = color;
+			Description = description;
 		}
 
 		public Label(string name, string color, string description) : this(name, color, description, System.Guid.NewGuid().ToString("D"))
